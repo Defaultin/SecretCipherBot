ec6dac1410816f988bf667aed1abd640bb26d833
Kirill <50778301+Defaultin@users.noreply.github.com>
2019-08-16 12:01:04 +0200

    Update data_tools.py

--- data_tools.py
+++ data_tools.py
@@ -3,13 +3,13 @@ import pickle
 
 
 def save_dataset(dataset):
-    in_file = open('dataset.txt', 'w')
+    in_file = open('dataset.txt', 'wb')
     pickle.dump(dataset, in_file)
     in_file.close()
 
 
 def load_dataset():
-    out_file = open('dataset.txt', 'r')
+    out_file = open('dataset.txt', 'rb')
     dataset = pickle.load(out_file)
     out_file.close()
     return dataset
